c62ebafb27be09acc758a6cd83bc029a6df25ec4
Vladimir Petrenko <vladipetrenko@vp-byte.de>
2017-02-17 11:34:57 +0100

    Added hall.blend to assets

--- JWGame/src/com/jw/game/minigames/quiz/data/QuizData.java
+++ JWGame/src/com/jw/game/minigames/quiz/data/QuizData.java
@@ -7,7 +7,7 @@ public class QuizData {
 
     public static List<String> lines() {
         List<String> lines = new ArrayList<>();
-        lines.add("veryeasy;How many children had Noah?;a:1;b:3;c:7;d:12;b");
+        lines.add("veryeasy;How many children had Noah?;a:one;b:tree;c:seven;d:ten;b");
         lines.add("veryeasy;Where live Jehova?;a:Haven;b:House;c:Temple;d:Sea;a");
         lines.add("easy;Who was the long livers men?;a:Noah;b:Isaak;c:Mathusal;d:Moses;c; Mathusal was 987 jears old");
         return lines;
