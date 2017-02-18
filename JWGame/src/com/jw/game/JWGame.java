38695fefcaa4c42365172ca5f69f97cc8e178ccb
Vladimir Petrenko <vladipetrenko@vp-byte.de>
2017-02-18 20:21:05 +0100

    Add bmfont to generate fonts for JME3 Latin, Cyrillic and other...

--- JWGame/src/com/jw/game/JWGame.java
+++ JWGame/src/com/jw/game/JWGame.java
@@ -2,12 +2,17 @@ package com.jw.game;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Spatial;
 import com.jw.game.levels.main.MainScene;
 import com.jw.game.minigames.quiz.QuizScene;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 
-public class JWGame extends SimpleApplication {
+public class JWGame extends SimpleApplication implements ScreenController {
+
     private MainScene mainScene;
     private QuizScene quizScene;
     boolean test = false;
@@ -24,13 +29,28 @@ public class JWGame extends SimpleApplication {
     public void simpleInitApp() {
         mainScene = new MainScene();
         quizScene = new QuizScene();
-        
+
         stateManager.attach(mainScene);
 
         Spatial blueflower = assetManager.loadModel("Models/plants/blueflower/blueflower.j3o");
         blueflower.setName("blueflower");
         rootNode.attachChild(blueflower);
+        
+/*        
+        NiftyJmeDisplay niftyDisplay = new NiftyJmeDisplay(assetManager,
+                inputManager,
+                audioRenderer,
+                guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        nifty.fromXml("Interface/quiz/quiz.xml", "start", this);
 
+        // attach the nifty display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay);
+
+        // disable the fly cam
+//        flyCam.setEnabled(false);
+//        flyCam.setDragToRotate(true);
+        inputManager.setCursorVisible(true);*/
     }
 
     @Override
@@ -44,5 +64,20 @@ public class JWGame extends SimpleApplication {
 
     @Override
     public void simpleRender(RenderManager rm) {
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+        System.out.println("bind( " + screen.getScreenId() + ")");
+    }
+
+    @Override
+    public void onStartScreen() {
+        System.out.println("onStartScreen");
+    }
+
+    @Override
+    public void onEndScreen() {
+        System.out.println("onEndScreen");
     }
 }
